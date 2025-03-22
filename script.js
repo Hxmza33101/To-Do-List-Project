a1f82f529912735bad49d3ffe53e226fe41ea5aa
Hxmza33101 <hkmughal2007@gmail.com>
2025-03-22 14:55:44 +1100

    added remove task button

--- script.js
+++ script.js
@@ -4,5 +4,29 @@ let CompletedL = []
 
 
 function addTask() {
-    
+    const taskInput = document.getElementById('To-do-list').value
+    if (taskInput) {
+        notStartedL.push(taskInput);
+        
+        console.log(notStartedL);
+        document.getElementById('To-do-list').value = '';
+
+        const taskList = document.getElementById('task-list');
+        const li = document.createElement('li');
+        li.textContent = taskInput;
+        
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = ' ';
+        deleteBtn.onclick = () => {
+            const index = notStartedL.indexOf(taskInput);
+            if (index > -1) {
+                notStartedL.splice(index, 1); // Remove from array
+            }
+            taskList.removeChild(li); // Remove from HTML
+        };
+
+        li.appendChild(deleteBtn);
+
+        taskList.appendChild(li);
+    }
 }
